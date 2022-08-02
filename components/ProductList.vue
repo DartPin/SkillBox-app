f9daf11b54cb14c61b80e7e60a03d5d029d4b317
DartPin <34026161+DartPin@users.noreply.github.com>
2022-08-02 21:38:50 +0300

    ДЗ 5.3
    добавлены хлебные крошки.
    Информация о товаре выводится согласно выбранному товару.
    Создан и реализован фильтр numberFormat

---
+++ components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="catalog"> 
+        <ul class="catalog__list">
+            <ProductItem v-for="product in products" :key="product.id" :product="product"></ProductItem>
+        </ul>
+    </section>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+    export default {
+    props: ["products"],
+    components: { ProductItem }
+}
+</script>
